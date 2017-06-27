50e3411607dda4e6415931166a25e5d87f4298ea
Erin K. Kennon <ekkennon@gmail.com>
2017-06-27 08:14:29 -0500

    basic outline of program

--- Dec2Bin/Dec2Bin.cpp
+++ Dec2Bin/Dec2Bin.cpp
@@ -1,11 +1,36 @@
 // Dec2Bin.cpp : main project file.
 
 #include "stdafx.h"
+#include <iostream>
+#include <limits>
 
+using namespace std;
 using namespace System;
 
-int main(array<System::String ^> ^args)
+void ConvertByRecursion(long long n);
+long long getDecimal();
+
+int main()
 {
-    Console::WriteLine(L"Hello World");
+	long long n;
+	cout << "Welcome to the Decimal to Binary Converter" << endl;
+	n = getDecimal();
+	while (n != 0) {
+		ConvertByRecursion(n);
+		cout << endl << endl;
+		n = getDecimal();
+	}
+	cout << "\nThanks for using the decimal converter";
+	system("Pause");
     return 0;
+}
+
+long long getDecimal() {
+	long long n;
+	
+	return n;
+}
+
+void ConvertByRecursion(long long n) {
+
 }
